6b0fbf4439a34be685d3c0ea9d2445f1b214fe12
Ubuntu <ubuntu@ip-172-31-69-20.ec2.internal>
2020-11-17 21:32:07 +0000

    Updated remote-build and added local-run

---
+++ c-hello-world/local-run.sh
@@ -0,0 +1,1 @@
+docker run --rm -it --platform linux/arm64 pareenaverma/c-hello-world-a1
--- c-hello-world/remote-build.sh
+++ c-hello-world/remote-build.sh
@@ -1,5 +1,9 @@
 docker context create remote --docker "host=ssh://ubuntu@3.83.150.252"
 docker context use remote
 
-./build.sh
+export DOCKER_BUILDKIT=1
+docker buildx create --name awsbuilder --use remote --platform linux/arm64, linux/arm/v7
+docker buildx use awsbuilder
+docker buildx build --platform linux/arm64,linux/arm/v7 -t pareenaverma/c-hello-world-a1 --push .
+
 
