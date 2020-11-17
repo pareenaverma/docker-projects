4a3a0b41f6d20967563bcd2dca2b8a605fe6cd1a
Ubuntu <ubuntu@ip-172-31-69-20.ec2.internal>
2020-11-17 16:35:35 +0000

    updated build.sh to include amd64

--- c-hello-world/build.sh
+++ c-hello-world/build.sh
@@ -3,5 +3,5 @@
 export DOCKER_BUILDKIT=1
 docker buildx create --name mybuilder
 docker buildx use mybuilder
-docker buildx build --platform linux/arm64,linux/arm/v7 -t pareenaverma/c-hello-world --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t pareenaverma/c-hello-world --push .
 
